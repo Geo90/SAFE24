a8678e2c9c8c735790158c9497a2b6873f2d0016
Yurdaer <yurdaer.dalkic@hotmail.com>
2017-02-11 17:15:18 +0100

    Tagit bort onödigafiler o metoder

--- Programkod/Yurdaer_test/test_4/manageCamera.cpp
+++ Programkod/Yurdaer_test/test_4/manageCamera.cpp
@@ -34,36 +34,7 @@ String sendToCamera ( String command, String host ) {
 
 }
 
-String getTime() {
-  WiFiClient client;
-  while (!!!client.connect("google.com", 80)) {
-    Serial.println("connection failed, retrying...");
-  }
 
-  client.print("HEAD / HTTP/1.1\r\n\r\n");
-  while(!!!client.available()) {
-     yield();
-  }
-
-  while(client.available()){
-    if (client.read() == '\n') {    
-      if (client.read() == 'D') {    
-        if (client.read() == 'a') {    
-          if (client.read() == 't') {    
-            if (client.read() == 'e') {    
-              if (client.read() == ':') {    
-                client.read();
-                String theDate = client.readStringUntil('\r');
-                client.stop();
-                return theDate;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 /**
    Controlling the camera movement by passing in int arguments for the desired angle
@@ -97,10 +68,7 @@ String continuousPanTiltMove (int panSpeed, int tiltSpeed, int camera_no){
   return url;
 }
 
-String GetTimeCamera () {
-  String url = "/axis-cgi/date.cgi?action=get";
-  return url;
-}
+
 
 
 
