23a46c02b76bb2d0551b9960f9bf135cc883f155
Unknown <benjo_953@hotmail.com>
2017-02-13 13:32:28 +0100

    Nyaste versionen
    Kommenterat

--- Programkod/louay/louay_test/sensors/sensors.ino
+++ Programkod/louay/louay_test/sensors/sensors.ino
@@ -1,73 +1,76 @@
 #include <Ethernet.h>
 #include <ESP8266WiFi.h>
 
-const int sensorPin = A0; // 
-const int ledPin1 = 14; // 
+const int sensorPin = A0; // Analogpin for the pir sensor 
+const int ledPin1 = 14; // Big lamp 
+const int ledPin2 = 12; // Small lamp
+const int pir = 13; //  Pir sensor
+unsigned long prevTime; // variable that keeps track of a previous time 
 
-const int ledPin2 = 12;
-const int pir = 13;
-unsigned long prevTime;
-unsigned long passTime;
-
-int sensorValue = 0; //value read from the PIR sensor
-int pirCounter = 0; //what does this count?
-int pirSum = 0; //waht is this
-
+int sensorValue = 0; // value read from the PIR sensor
+int pirCounter = 0; // counter to read from pir sensor
+int pirSum = 0; // sum to add up the HIGHs and LOWs from pir sensor
 
 /**
  * Initializing the pins
  */
 void setup() 
 {
-  pinMode(ledPin1, OUTPUT); //pin1
-  pinMode(ledPin2, OUTPUT); //pin1
+  pinMode(ledPin1, OUTPUT); 
+  pinMode(ledPin2, OUTPUT); 
   pinMode(pir, INPUT);
   pinMode(sensorPin, INPUT);
-  prevTime = passTime = 0;
+  prevTime = 0;
   Serial.begin(115200);
 }
 
-
+/*
+ * uses a reading from pir sensor to calculate a movement
+ */
 void doWithPirValue(int pirvalue) {
-  if (pirvalue == HIGH ){
-    //digitalWrite(ledPin1, HIGH);
+  if (pirvalue == HIGH){
     pirCounter = pirCounter + 1;
     pirSum = pirSum + 10;
   }
 
-  
-  if (pirvalue == LOW ){
-    //digitalWrite(ledPin1, LOW);
+  if (pirvalue == LOW){
     pirCounter = pirCounter + 1;
   }
 }
 
+/*
+ * checks if there is a movement or not and acts according to that
+ */
 void doWhenMove() {
   if (pirCounter == 7) {
     pirCounter = 0;
     if (pirSum == 70) {
       prevTime = millis();
       digitalWrite(ledPin1, HIGH);
-      passTime = 0;
     }
     if (pirSum < 70 && ( millis()  - prevTime ) > 10000 ) {
       digitalWrite(ledPin1, LOW);
       prevTime = 0;
-      passTime = 0;
     }
     pirSum = 0;
   }  
 }
 
+/*
+ * checks if the sensor value goes past the threshold of 75. 
+ */
 void doWithSensorValue(int sensorvalue) {
-  if (sensorvalue > 75) {
+  if (sensorvalue > 75) { // LED ON 
       digitalWrite(ledPin2, HIGH);
   }
-  if (sensorvalue < 75) {
+  if (sensorvalue < 75) { // LED OFF
       digitalWrite(ledPin2, LOW);
   }
 }
 
+/*
+ * loops continously in order to obtain the newest sensor values  
+ */
 void loop() 
 { 
   Serial.begin(115200);
