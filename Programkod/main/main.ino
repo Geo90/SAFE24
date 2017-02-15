e63409d42347f221e8e3cf5a8b4644707d6c3c46
Yurdaer <yurdaer.dalkic@hotmail.com>
2017-02-15 17:02:42 +0100

    Skapat en metod som ska anropas för att aktivera events

--- Programkod/main/main.ino
+++ Programkod/main/main.ino
@@ -16,6 +16,7 @@ const String camera_ip = "192.168.0.70";
 
 const String guardTourPort = "9";
 const String recordPort = "1";
+const String StationOnePort = "2";
 const char* username = "root";
 const char* password = "pass";
 
@@ -37,28 +38,34 @@ void setup() {
 
   // Connecting to a WiFi network
   connectWifi(esp_ip, dns, gateway, subnet, ssid, passwordWifi);
+delay(500);
+  activateCamera();
 }
 
 void loop() {
 
-  sendToCamera(camera_ip, activateVirtualPort (recordPort), username, password);
-  delay(100);
-  Serial.println(sendToCamera(camera_ip, activateVirtualPort (guardTourPort), username, password));
 
-  delay(20000);
-
-  Serial.println(sendToCamera(camera_ip, deactivateVirtualPort (guardTourPort), username, password));
-  delay(100);
-  sendToCamera(camera_ip, deactivateVirtualPort (recordPort), username, password);
-
-while(1){
-  
-}
   // Try to connect the WiFi network as long as the connection is down
+  /*
   while (!checkConnection()) {
     Serial.println("Wifi connection is down");
     connectWifi(esp_ip, dns, gateway, subnet, ssid, password);
   }
+  */
+}
+void activateCamera(){
+ // sendToCamera(camera_ip, activateVirtualPort (StationOnePort), username, password);
+  delay(50);
+  sendToCamera(camera_ip, activateVirtualPort (recordPort), username, password);
+  delay(50);
+  sendToCamera(camera_ip, activateVirtualPort (guardTourPort), username, password);
+for(int i=0;i<60;i++){
+  delay(1000);
+}
+  sendToCamera(camera_ip, deactivateVirtualPort (guardTourPort), username, password);
+  delay(100);
+  sendToCamera(camera_ip, deactivateVirtualPort (recordPort), username, password);
+ // sendToCamera(camera_ip, deactivateVirtualPort (StationOnePort), username, password);
 }
 
 
